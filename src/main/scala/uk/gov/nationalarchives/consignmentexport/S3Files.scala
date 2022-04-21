e3c6f60960cba44cb5757f5ac9dbfb6c7b573576
TomJKing <tom.king@nationalarchives.gov.uk>
2022-04-21 06:42:54 +0100

    Download files in batches to even out s3 requests
    Trying to download all the files for a large consignments uses up all the s3 connections causing the download to fail
    
    Batching with a pause even outs the burst of request to s3. Pausing on batches is also quicker that pausing on each individual file download request

--- src/main/scala/uk/gov/nationalarchives/consignmentexport/S3Files.scala
+++ src/main/scala/uk/gov/nationalarchives/consignmentexport/S3Files.scala
@@ -5,32 +5,47 @@ import java.util.UUID
 
 import cats.effect.IO
 import cats.implicits._
+import com.typesafe.config.ConfigFactory
 import org.typelevel.log4cats.SelfAwareStructuredLogger
+import software.amazon.awssdk.services.s3.model.GetObjectResponse
 import uk.gov.nationalarchives.aws.utils.S3Utils
 import uk.gov.nationalarchives.consignmentexport.Main.directoryType
 import uk.gov.nationalarchives.consignmentexport.Utils._
 import uk.gov.nationalarchives.consignmentexport.Validator.ValidatedFileMetadata
-import scala.concurrent.duration._
 
+import scala.concurrent.duration._
 import scala.language.postfixOps
 
 class S3Files(s3Utils: S3Utils)(implicit val logger: SelfAwareStructuredLogger[IO]) {
+  private val configuration = ConfigFactory.load()
+  private val downloadBatchSize = configuration.getInt("s3.downloadFilesBatchSize")
+  private val downloadDelay = configuration.getInt("s3.downloadBatchDelayMs")
 
-  def createDownloadDirectories(files: List[ValidatedFileMetadata], consignmentReference: String, rootLocation: String) = {
+  def createDownloadDirectories(files: List[ValidatedFileMetadata], consignmentReference: String, rootLocation: String): IO[List[Boolean]] = {
     IO {
       new File(s"$rootLocation/$consignmentReference").mkdirs()
       files.filter(_.fileType == "Folder").map(f => new File(s"$rootLocation/$consignmentReference/${f.clientSideOriginalFilePath}").mkdirs())
     }
   }
 
-  def downloadFiles(files: List[ValidatedFileMetadata], bucket: String, consignmentId: UUID, consignmentReference: String, rootLocation: String): IO[Unit] = for {
-    _ <- createDownloadDirectories(files, consignmentReference, rootLocation)
-    _ <- files.filter(_.fileType != directoryType).map(file => {
-      IO.sleep(100 milliseconds).flatMap(_ =>
-        s3Utils.downloadFiles(bucket, s"$consignmentId/${file.fileId}", s"$rootLocation/$consignmentReference/${file.clientSideOriginalFilePath}".toPath.some))
-    }).sequence
-    _ <- logger.info(s"Files downloaded from S3 for consignment $consignmentId")
-  } yield ()
+  def batchDownloadingFiles(batch: List[ValidatedFileMetadata], bucket: String, consignmentId: UUID, consignmentReference: String, rootLocation: String): IO[List[GetObjectResponse]] = {
+    IO.sleep(downloadDelay milliseconds).flatMap(_ => {
+      batch.map(file =>
+        s3Utils.downloadFiles(
+          bucket,
+          s"$consignmentId/${file.fileId}",
+          s"$rootLocation/$consignmentReference/${file.clientSideOriginalFilePath}".toPath.some
+        )).sequence
+    })
+  }
+
+  def downloadFiles(files: List[ValidatedFileMetadata], bucket: String, consignmentId: UUID, consignmentReference: String, rootLocation: String): IO[Unit] = {
+    for {
+      _ <- createDownloadDirectories(files, consignmentReference, rootLocation)
+      _ <- files.filter(_.fileType != directoryType).grouped(downloadBatchSize).toSeq.map(batchDownloadingFiles(_, bucket, consignmentId, consignmentReference, rootLocation)).sequence
+      _ <- logger.info(s"Files downloaded from S3 for consignment $consignmentId")
+    } yield()
+  }
 
   def uploadFiles(bucket: String, consignmentId: UUID, consignmentReference: String, tarPath: String): IO[Unit] = for {
     _ <- s3Utils.upload(bucket, s"$consignmentReference.tar.gz", tarPath.toPath)
