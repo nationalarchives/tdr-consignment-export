6fc1cdf9263ddca7112a9977f8c4cf65d019953d
tna-digital-archiving-jenkins <digitalpreservation@nationalarchives.gov.uk>
2022-04-28 08:25:16 +0000

    Update tdr-auth-utils to 0.0.48

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -7,7 +7,7 @@ object Dependencies {
   private val mockitoScalaVersion = "1.17.5"
   private val monovoreDeclineVersion = "2.2.0"
 
-  lazy val authUtils = "uk.gov.nationalarchives" %% "tdr-auth-utils" % "0.0.47"
+  lazy val authUtils = "uk.gov.nationalarchives" %% "tdr-auth-utils" % "0.0.48"
   lazy val awsUtils =  "uk.gov.nationalarchives" %% "tdr-aws-utils" % "0.1.29"
   lazy val bagit = "gov.loc" % "bagit" % "5.2.0"
   lazy val catsEffect = "org.typelevel" %% "cats-effect" % "3.3.11"
