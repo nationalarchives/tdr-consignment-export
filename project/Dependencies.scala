889a6bc13a2490876ccb2217da9ad37ae0dff512
tna-digital-archiving-jenkins <digitalpreservation@nationalarchives.gov.uk>
2022-05-11 08:25:15 +0000

    Update tdr-generated-graphql to 0.0.241

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -7,7 +7,7 @@ object Dependencies {
   private val mockitoScalaVersion = "1.17.5"
   private val monovoreDeclineVersion = "2.2.0"
 
-  lazy val generatedGraphql = "uk.gov.nationalarchives" %% "tdr-generated-graphql" % "0.0.240"
+  lazy val generatedGraphql = "uk.gov.nationalarchives" %% "tdr-generated-graphql" % "0.0.241"
   lazy val authUtils = "uk.gov.nationalarchives" %% "tdr-auth-utils" % "0.0.50"
   lazy val awsUtils =  "uk.gov.nationalarchives" %% "tdr-aws-utils" % "0.1.29"
   lazy val bagit = "gov.loc" % "bagit" % "5.2.0"
