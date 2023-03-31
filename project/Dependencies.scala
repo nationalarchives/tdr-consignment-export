68a89dd294a889644f5bc73248f5eb41ed40b405
tna-digital-archiving-jenkins <digitalpreservation@nationalarchives.gov.uk>
2023-03-31 08:27:42 +0000

    Update keycloak-admin-client, ... to 21.0.2

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -2,7 +2,7 @@ import sbt._
 
 object Dependencies {
   private val githubPureConfigVersion = "0.17.2"
-  private val keycloakVersion = "21.0.1"
+  private val keycloakVersion = "21.0.2"
   private val log4CatsVersion = "2.5.0"
   private val mockitoScalaVersion = "1.17.14"
   private val monovoreDeclineVersion = "2.4.1"
