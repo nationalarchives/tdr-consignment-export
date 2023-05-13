0b8261357fd8ffb4ec8de6a9009e34b9d321cb78
tna-digital-archiving-jenkins <digitalpreservation@nationalarchives.gov.uk>
2023-05-13 08:27:42 +0000

    Update pureconfig, pureconfig-cats-effect to 0.17.4

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -1,7 +1,7 @@
 import sbt._
 
 object Dependencies {
-  private val githubPureConfigVersion = "0.17.3"
+  private val githubPureConfigVersion = "0.17.4"
   private val keycloakVersion = "21.1.1"
   private val log4CatsVersion = "2.6.0"
   private val mockitoScalaVersion = "1.17.14"
