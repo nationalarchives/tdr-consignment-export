95e8d0465199b988fc565751ebff03c576746443
tna-digital-archiving-jenkins <digitalpreservation@nationalarchives.gov.uk>
2023-05-19 08:26:38 +0000

    Update s3-utils, stepfunction-utils to 0.1.88

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -6,7 +6,7 @@ object Dependencies {
   private val log4CatsVersion = "2.6.0"
   private val mockitoScalaVersion = "1.17.14"
   private val monovoreDeclineVersion = "2.4.1"
-  private val awsUtilsVersion = "0.1.86"
+  private val awsUtilsVersion = "0.1.88"
 
   lazy val authUtils = "uk.gov.nationalarchives" %% "tdr-auth-utils" % "0.0.132"
   lazy val generatedGraphql = "uk.gov.nationalarchives" %% "tdr-generated-graphql" % "0.0.324"
