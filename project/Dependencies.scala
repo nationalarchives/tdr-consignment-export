d4a86a3609f2e756d106b2f3e8307cb8542ecc87
tna-digital-archiving-jenkins <digitalpreservation@nationalarchives.gov.uk>
2023-08-31 08:29:07 +0000

    Update mockito-scala, ... to 1.17.22

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -4,7 +4,7 @@ object Dependencies {
   private val githubPureConfigVersion = "0.17.4"
   private val keycloakVersion = "22.0.1"
   private val log4CatsVersion = "2.6.0"
-  private val mockitoScalaVersion = "1.17.14"
+  private val mockitoScalaVersion = "1.17.22"
   private val monovoreDeclineVersion = "2.4.1"
   private val awsUtilsVersion = "0.1.97"
 
