003a109736d8ae461e87614bacd7e5a1ab34b6b4
tna-digital-archiving-jenkins <digitalpreservation@nationalarchives.gov.uk>
2024-09-19 08:25:33 +0000

    Update doobie-core, doobie-postgres to 1.0.0-RC6 in scala-steward-dependencies

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -7,7 +7,7 @@ object Dependencies {
   private val mockitoScalaVersion = "1.17.37"
   private val monovoreDeclineVersion = "2.4.1"
   private val awsUtilsVersion = "0.1.205"
-  private val doobieVersion = "1.0.0-RC5"
+  private val doobieVersion = "1.0.0-RC6"
   private val testContainersVersion = "0.41.4"
 
   lazy val authUtils = "uk.gov.nationalarchives" %% "tdr-auth-utils" % "0.0.213"
